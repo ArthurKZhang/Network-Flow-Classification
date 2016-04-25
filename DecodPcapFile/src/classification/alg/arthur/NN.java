3929aaec1b14e7e70dabe26f366324515ffcf99f
ArthurKZhang <arthurzhang73@gmail.com>
2016-04-25 23:14:41 +0800

    Three distance judging method
    AVG, MIN, MVT. can be used in Bayes-combinded-NN Alg

---
+++ DecodPcapFile/src/classification/alg/arthur/NN.java
@@ -0,0 +1,48 @@
+package classification.alg.arthur;
+
+import java.util.List;
+import java.util.Map;
+
+public class NN {
+	public static double avgDistance(List<String> x, List<List<String>> tx) {
+		double dis = 0;
+		for (List<String> l : tx) {
+			dis += distance(x, l);
+		}
+		return BigDecimalCalculate.div(dis, tx.size());
+	}
+
+	public static double minDistance(List<String> x, List<List<String>> tx) {
+		double dis = Double.MAX_VALUE;
+		double temp;
+		for (List<String> l : tx) {
+			if ((temp = distance(x, l)) < dis)
+				dis = temp;
+		}
+		return dis;
+	}
+
+	public static double mvt(List<String> x, String tw, Map<String, List<List<String>>> mtx) {
+		// majority vote//
+		double wdis = Double.MAX_VALUE;
+		String s = null;
+		for (String w : mtx.keySet()) {
+			double dis = Double.MAX_VALUE;
+			double temp;
+			for (List<String> tx : mtx.get(w)) {
+				if ((temp = distance(x, tx)) < dis)
+					dis = temp;
+			}
+			if (wdis > dis) {
+				wdis = dis;
+				s = w;
+			}
+		}
+		return s == tw ? 1 : 0;
+	}
+
+	private static double distance(List<String> x, List<String> l) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
