5a4eebdaa344de56d04a0237e6d7ef9de6f2f3b6
ArthurKZhang <arthurzhang73@gmail.com>
2016-05-04 23:45:19 +0800

    ```````

---
+++ DecodPcapFile/src/data/alg/arthur/DataCreationAlgorithm.java
@@ -0,0 +1,14 @@
+package data.alg.arthur;
+
+import data.bean.arthur.IPPacket;
+
+/**
+ * Create data bean by pcap data bean
+ * @author Arthur
+ *
+ */
+public class DataCreationAlgorithm {
+	public static IPPacket pcap2IPPacket(){
+		return null;
+	}
+}
